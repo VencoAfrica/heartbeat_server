ef52493afc7d40d6d81e6c49f6337b4fcdc2b41e
ckosiegbu <chude.osiegbu@manqala.com>
2020-10-31 18:54:18 +0100

    .

--- heartbeat_server.py
+++ heartbeat_server.py
@@ -21,13 +21,57 @@ def _load_config(filename="config.json"):
         print("Failed to load configuration files")
     pass
 
+def _parse_heartbeat(in_data):
+	# version_number_size = 2
+	# source_address_size = 2
+	# target_address_size = 2
+	# frame_length_size = 2
+	# fixed_format_size = 5
+	# structure_size = 2
+	# visible_string_size = 1
+	# device_details_length_size = 1
+	# device_details_size = value of device_details_length converted to int
+	# double_long_unsigned_size = 1
+	# address_size = 4
+	
+	try:
+		version_number = in_data[0:2]
+		source_address = in_data[2:4]
+		target_address = in_data[4:6]
+		frame_length = in_data[6:8]
+		fixed_format = in_data[8:13]
+		structure = in_data[13:15]
+		visible_string = in_data[15:16]
+		device_details_length = in_data[16:17]
+		device_details_length_int = int.from_bytes(device_details_length,byteorder ='big')
+		device_details = in_data[17:17+device_details_length_int]
+		double_long_unsigned = in_data[17+device_details_length_int:17+device_details_length_int+1]
+		address = in_data[17+device_details_length_int+1:17+device_details_length_int+5]
+	
+		#create out_data remember to transports the target_address and source_address
+		out_data = version_number+target_address+source_address+frame_length+fixed_format
+		
+		print('version_number: ', bytearray(version_number).hex())
+		print('source_address: ', bytearray(target_address).hex())
+		print('target_address: ', bytearray(source_address).hex())
+		print('frame_length: ', bytearray(frame_length).hex())
+		print('fixed_format: ', bytearray(fixed_format).hex())
+		
+		print('heartbeat response: ', bytearray(out_data).hex())
+	except IndexError as err:
+		print("badly formed heartbeat. cannot log or respond!")
+		print(err)
+		out_data = b'\xFF'
+		
+	return out_data
+
 def _client_service(conn, addr):
-    print('Connected by', addr)
+    print('Connected from', addr)
     with conn:
         while True:
             data = conn.recv(1024)
             if not data: break
-            conn.send(data)
+            conn.send(_parse_heartbeat(data))
     print("Goodbye ", addr)
     pass
 
@@ -55,14 +99,6 @@ def _listener(af, socktype, proto, sa):
         print(err)
         pass
 
-# def udp_client_service(udp_client):
-#     pass
-
-# def udp_listener():
-#     pass
-
-# def start():
-#     pass
 
 def signals_handler(signum, frame):
     print('Signal handler called with signal', signum)
@@ -86,13 +122,6 @@ if __name__ == "__main__":
     # TCP
     for res in socket.getaddrinfo(None, __config["tcp"]["port"], socket.AF_UNSPEC,
                                 socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-        af, socktype, proto, canonname, sa = res
-        print("Start listening for")
-        print((af, socktype, proto, sa,))
-        __active_threads.put(_thread.start_new_thread(_listener, (af, socktype, proto, sa,)))
-    # UDP
-    for res in socket.getaddrinfo(None, __config["udp"]["port"], socket.AF_UNSPEC,
-                                socket.SOCK_DGRAM, 0, socket.AI_PASSIVE):
         af, socktype, proto, canonname, sa = res
         print("Start listening for")
         print((af, socktype, proto, sa,))
