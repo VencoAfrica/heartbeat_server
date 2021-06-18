bb74a8c4304766c867bacf10e7f1fbf9201c7f14
crossxcell99 <crossxcell99@gmail.com>
2021-06-18 11:44:19 +0100

    fix use of 0x1F in getting mac_l mac_h

--- heartbeat_server/parser.py
+++ heartbeat_server/parser.py
@@ -159,41 +159,47 @@ def get_meter_no(meter):
     meter_reversed = [meter[i-2: n+i] for i in range(0, -n, -2)]
     return bytearray(int(i, 16) for i in meter_reversed)
 
-def get_mac(password, low_or_high='L'):
+def get_mac(random_no, password, low_or_high='L'):
+    if not isinstance(random_no, bytes):
+        random_no = (int(str(random_no))).to_bytes(1, 'big')
+
     if not isinstance(password, (bytes, bytearray)):
         password = bytes(password,'utf-8')
 
     arr = bytearray()
     if low_or_high == 'L':
-        arr += b'\x1F' + password
+        arr += random_no + password
     elif low_or_high == 'H':
-        arr += password + b'\x1F'
+        arr += password + random_no
     else:
         raise TypeError('low_or_high should be "H" or "L"')
     crc = CRC(b'\xA5', arr)
     return (int.from_bytes(crc, 'big') + 0x33).to_bytes(2, 'big')[-1:]
 
 def prep_data(meter_no, pass_lvl, random_no, passw, data):
-	if not isinstance(pass_lvl, bytes):
-		pass_lvl = (int(str(pass_lvl)) + 0x33).to_bytes(1, 'big')
+    if not isinstance(random_no, bytes):
+        random_no = (int(str(random_no))).to_bytes(1, 'big')
 
-	if not isinstance(random_no, bytes):
-		random_no = (int(str(random_no)) + 0x33).to_bytes(1, 'big')
+    if not isinstance(pass_lvl, bytes):
+        pass_lvl = (int(str(pass_lvl)) + 0x33).to_bytes(1, 'big')
 
-	# 77 77 PA 52 MAC_L MAC_H, length = 6
-	length = 6 + len(data)
-	out = bytearray([0x68])
-	out += get_meter_no(meter_no)
-	out += bytearray([0x68, 0x01])
-	out += length.to_bytes(1, 'big')
-	out += bytearray([0x77, 0x77])
-	out += pass_lvl
-	out += random_no
-	out += get_mac(passw, 'L')
-	out += get_mac(passw, 'H')
-	out += bytearray(0x33 + i for i in data)
+    random_no_padded = (int.from_bytes(
+        random_no, 'big') + 0x33).to_bytes(1, 'big')
 
-	data_sum = sum(out)
-	out += data_sum.to_bytes(2, 'big')[-1:]
-	out += bytearray([0x16])
-	return out
+    # 77 77 PA 52 MAC_L MAC_H, length = 6
+    length = 6 + len(data)
+    out = bytearray([0x68])
+    out += get_meter_no(meter_no)
+    out += bytearray([0x68, 0x01])
+    out += length.to_bytes(1, 'big')
+    out += bytearray([0x77, 0x77])
+    out += pass_lvl
+    out += random_no_padded
+    out += get_mac(random_no, passw, 'L')
+    out += get_mac(random_no, passw, 'H')
+    out += bytearray(0x33 + i for i in data)
+
+    data_sum = sum(out)
+    out += data_sum.to_bytes(2, 'big')[-1:]
+    out += bytearray([0x16])
+    return out
