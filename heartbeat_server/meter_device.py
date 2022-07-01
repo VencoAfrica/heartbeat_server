9b20413aa5ac6402314db482adb0cb6488db9177
reagan <reagan@nectar.software>
2022-07-01 10:12:03 +0300

    refactored management of redis keys

--- heartbeat_server/meter_device.py
+++ heartbeat_server/meter_device.py
@@ -11,6 +11,7 @@ import redis
 
 READ = 'Read'
 WRITE = 'Write'
+BULK_WRITER_DELIMITER = '*|'
 
 
 def get_reading_cmds(redis_params):
@@ -19,7 +20,7 @@ def get_reading_cmds(redis_params):
             '*',
             cmd,
             CommandMessage.for_single_read(obis_code).to_bytes()
-         )
+        )
         for cmd, obis_code in obis_codes.items()
     ]
     return read_commands + get_write_commands(redis_params)
@@ -35,13 +36,16 @@ def get_write_commands(redis_params):
     r = redis.Redis(host=redis_params.get('host', '0.0.0.0'),
                     port=redis_params.get('port', 6379),
                     db=redis_params.get('db', 0))
-    for meter_no in r.scan_iter("*"):
-        command = str(r.get(meter_no), 'utf-8')
-        write_commands.append(
-            (
-                meter_no,
-                command.split(':')[0],
-                write_value(command.split(':')[0],
-                            command.split(':')[1]))
-        )
+    for key in r.scan_iter("*"):
+        if str(key, 'utf-8') \
+                .startswith(BULK_WRITER_DELIMITER):
+            command = str(r.get(key), 'utf-8')
+            write_commands.append(
+                (
+                    key,
+                    command.split(':')[0],
+                    write_value(command.split(':')[0],
+                                command.split(':')[1]))
+            )
+            r.delete(key)
     return write_commands
